4836d80e47d7651349e0e7dbd76715cf7d56f57f
Daisuke Orita <kikoricontact@yahoo.co.jp>
2020-02-22 14:24:59 +0900

    Initial Commit

---
+++ LightningGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LightningGame
+//
+//  Created by Daisuke Orita on 2020/02/22.
+//  Copyright © 2020 Daisuke Orita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
